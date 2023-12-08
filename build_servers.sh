416a444fe8357aa557cdc831ab254588082fd0f9
helgesander <helgesander1006@list.ru>
2023-12-08 16:46:58 +0300

    Fix buiding script

--- build_servers.sh
+++ build_servers.sh
@@ -7,7 +7,12 @@ fi
 
 sudo apt install libx11-dev
 
-make -B
+distrib=`awk -F "DISTRIB_ID=" '/DISTRIB_ID=/ {print $2; exit}' /etc/lsb-release`
+if [ $distrib = "Ubuntu" ]; then
+    make -f Makefile-debian-ubuntu -B
+else
+    make -B
+fi
 
 ./second_log & 
 ./second_server &
